d8f9172ad6a2f07b41d4f8760ecc5a5385e32353
ghiocgeorge <ghiocgeorge@gmail.com>
2021-01-16 22:05:51 +0200

    Update the navbar

--- src/main/java/ro/vladutit/Don/t/forget/v2/model/CustomUserDetails.java
+++ src/main/java/ro/vladutit/Don/t/forget/v2/model/CustomUserDetails.java
@@ -28,6 +28,10 @@ public class CustomUserDetails implements UserDetails {
         return user.getEmail();
     }
 
+    public String getFullname() {
+        return this.user.getFullname();
+    }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
